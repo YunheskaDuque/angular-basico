8185eba07f8e6d0e1f8b6562825f17c05dcbcd66
Yunheska Isabel Duque Badell <yunheska.duque@softtekprojects.com>
2023-06-23 14:48:25 -0400

    fin de sección

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
-
-//crear un módulo llamado ContadorModule
-//declaraciones y exportaciones
-
 import { HeroesModule } from './heroes/heroes.module';
 import { ContadorModule } from './contador/contador.module';
+import { DbzModule } from './dbz/dbz.module';
 
 
 @NgModule({
@@ -18,7 +15,8 @@ import { ContadorModule } from './contador/contador.module';
   imports: [
     BrowserModule,
     HeroesModule,
-    ContadorModule
+    ContadorModule,
+    DbzModule
     
   ],
   providers: [],
--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+
 import { ListadoComponent } from './listado/listado.component';
 import { HeroeComponent } from "./heroe/heroe.component";
 
